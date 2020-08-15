203360bd20c0328649d1adc7d13d4ad99bf9c9ed
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2020-08-15 11:21:08 +0300

    Added login form

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
@@ -13,7 +15,9 @@ import {EditPageComponent} from './edit-page/edit-page.component';
     LoginPageComponent,
     DashboardPageComponent,
     CreatePageComponent,
-    EditPageComponent
+    EditPageComponent,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   imports: [
     CommonModule,
