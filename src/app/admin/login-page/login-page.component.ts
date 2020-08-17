479b4fa71a4d657ad1551bb1ecd9fc201025a528
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2020-08-17 17:18:25 +0300

    Added auth guard

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
 import {AuthService} from '../shared/services/auth.service';
-import {Router} from '@angular/router';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,15 +12,23 @@ import {Router} from '@angular/router';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup
-  submitted: false
+  submitted: boolean
+  message: string
 
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute,
   ) {
   }
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['loginAgain']) {
+        this.message = 'You are not authorized'
+      }
+    })
+
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
