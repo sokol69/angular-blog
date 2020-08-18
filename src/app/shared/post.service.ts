f5010c3fa063b756196d1aa0667957dbe4dde1df
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2020-08-18 18:23:49 +0300

    Added post service

---
+++ src/app/shared/post.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
+import { Post, FbCreateResponse } from './interfaces';
+
+@Injectable({providedIn: 'root'})
+export class PostService {
+  constructor(private http: HttpClient) {
+  }
+
+  create(post: Post): Observable<Post> {
+    return this.http.post(`${environment.fbDbUrl}/posts.json`, post)
+      .pipe(map((res: FbCreateResponse) => {
+        return {
+          ...post,
+          id: res.name,
+          date: new Date(post.date),
+        };
+      }))
+  }
+}
