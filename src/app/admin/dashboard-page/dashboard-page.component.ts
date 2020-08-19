7f1b6097b0742998583356c2dbdfc6b4ccf59884
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2020-08-19 07:44:24 +0300

    Added post removing

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -13,6 +13,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = []
   pSub: Subscription
+  dSub: Subscription
   searchStr = ''
 
   constructor(private postsService: PostService) { }
@@ -27,10 +28,16 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     if (this.pSub) {
       this.pSub.unsubscribe();
     }
+
+    if (this.dSub) {
+      this.dSub.unsubscribe();
+    }
   }
 
   remove(id: string) {
-
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter((post) => post.id !== id);
+    })
   }
 
 }
