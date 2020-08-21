be53b60154e25c10478320cbe3434fa3d344b03c
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2020-08-21 07:41:38 +0300

    Finished post editing

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { PostService } from 'src/app/shared/post.service';
 import { switchMap } from 'rxjs/operators';
 import { Post } from 'src/app/shared/interfaces';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup
+  post: Post
+  submited = false
+
+  uSub: Subscription
 
   constructor(
     private route: ActivatedRoute,
@@ -25,10 +30,33 @@ export class EditPageComponent implements OnInit {
         return this.postService.getById(params['id']);
       })
     ).subscribe((post: Post) => {
+      this.post = post;
       this.form = new FormGroup({
         title: new FormControl(post.title, Validators.required),
         text: new FormControl(post.text, Validators.required)
       })
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.uSub) {
+      this.uSub.unsubscribe();
+    }
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+
+    this.submited = true;
+
+    this.uSub = this.postService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title,
+    }).subscribe(() => {
+      this.submited = false;
     })
   }
 
--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -51,4 +51,8 @@ export class PostService {
   remove(id: string): Observable<void> {
     return this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`);
   }
+
+  update(post: Post): Observable<Post> {
+    return this.http.patch<Post>(`${environment.fbDbUrl}/posts/${post.id}.json`, post);
+  }
 }
